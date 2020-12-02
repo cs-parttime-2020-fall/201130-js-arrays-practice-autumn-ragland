a5626b170e56e9f3f221c301db90eadae59021e7
autumn-ragland <raglanda998@gmail.com>
2020-12-01 18:31:06 -0600

    completed walkthrouhg

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-// console.log("20 11 30 JS Arrays Practice"); // check that files are linked
+console.log("20 11 30 JS Arrays Practice"); // check that files are linked
 
-// EX 1 : declare array with fruit strings
+// EX 1
 let fruitArray = ["apple", "orange", "peach", "pear"]; // declare array
-console.log(fruitArray); // output to browser
+console.log(fruitArray); // output array
 
-// EX 2 : output array length
-let fruitLength = fruitArray.length; // determine length
-console.log(`There are ${fruitArray.length} fruits in the array`); // output message 
+// EX 2
+let fruitLength = fruitArray.length; // save length in variable
+console.log(`There are ${fruitLength} fruits in the array`); // output length
+// console.log(`There are ${fruitArray.length} fruits in the array`);
 
-// EX 3 : prompt user to guess length
-let userGuess = parseInt(prompt("How many items are in the fruit array")); // prompt user
-if(userGuess == fruitArray.length){ // correct guess
-    alert("You guessed correctly");
-} else if(userGuess < fruitLength){ // guess too low
-    alert(`You guessed too low. There are actually ${fruitArray.length} fruits in the array`);
-} else if(userGuess > fruitLength) { // guess to high
-    alert(`You guessed too high. There are actually ${fruitLength} fruits in the array`);
+// EX 3
+
+// Prompt the user to guess how many fruits are in the fruit array.
+let userGuess = parseInt(prompt("How many fruit are in the fruit array")); 
+
+if(userGuess == fruitArray.length){ //correct guess
+
+    alert("you guessed correctly"); 
+
+} else if(userGuess < fruitArray.length){ // too low
+
+    alert(`You guessed too low. There are actually ${fruitArray.length} in the array`);
+
+} else if(userGuess > fruitArray.length){ // too high
+
+    alert(`You guessed too high. There are actually ${fruitArray.length} in the array`);
 }
 
-// EX 4 : add fruit and output last fruit
-let userFruit = prompt("Enter a fruit!"); // prompt user
-fruitArray.push(userFruit); // add user fruit
-console.log(`The last fruit ${fruitArray[fruitArray.length - 1]} was added by the user`); // output last element
+// Prompt the user to enter a fruit. 
+let userFruit = prompt("Enter a fruit");
+
+// Add their fruit to the end of fruitArray. 
+fruitArray.push(userFruit);
+
+// Output the fruit added to the end of the array by index position in the formatted message : "The last fruit [LAST_ELEMENT_IN_ARRAY] was added by the user!"
+fruitLength = fruitArray.length; // update value of fruitLength
+console.log(`The last fruit ${fruitArray[fruitLength - 1]} was added by the user!`);
+
+
